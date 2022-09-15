975a9d953b6ecf87cff7390e0c7b10ba65defebb
jesse <jesse@JESSE>
2022-09-15 10:46:41 +0300

    Add project files.

---
+++ 150922/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Input your string, please: ");
+string yourInput = Console.ReadLine();
+int lenght = 0;
+
+if (yourInput != null)
+{
+    foreach (var item in yourInput)
+    {
+        lenght++;
+        //lenght = lenght + 1
+    }
+}
+Console.WriteLine("manually calculated: {0}.", lenght);
+
+//This was not allowed:
+Console.WriteLine("The lenght of your story was {0} characters.",
+yourInput.Length);
+
+
+
